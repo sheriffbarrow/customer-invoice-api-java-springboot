6b081598dc83d82f5192eadf8c1bf690596f41b0
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-06-24 15:28:18 +0000

    created user query package to handle userQuery class queries

---
+++ src/main/java/sheriff/customer/invoice/management/query/UserQuery.java
@@ -0,0 +1,6 @@
+package sheriff.customer.invoice.management.query;
+
+public class UserQuery {
+    public static final  String COUNT_USER_EMAIL_QUERY = "";
+    public static final String INSERT_USER_QUERY = "";
+}
