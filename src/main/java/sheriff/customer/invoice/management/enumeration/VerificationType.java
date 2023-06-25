2cd17c2ce6bd77a2a38dbab4a215df56c7050af2
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-06-25 16:39:20 +0000

    sending verification url

---
+++ src/main/java/sheriff/customer/invoice/management/enumeration/VerificationType.java
@@ -0,0 +1,24 @@
+package sheriff.customer.invoice.management.enumeration;
+
+public enum VerificationType {
+    ACCOUNT("ACCOUNT"),
+    PASSWORD("PASSWORD");
+
+    private final String type;
+
+    VerificationType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return this.type.toLowerCase();
+    }
+
+
+
+
+
+
+
+
+}
